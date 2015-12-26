b425ceb8cf0e204c66431cd1db67c3f7e59f54b8
Michael Laszlo <git@michaellaszlo.com>
2015-12-25 23:55:23 -0500

    Undo options.second hack in paintArc

--- clock.js
+++ clock.js
@@ -346,19 +346,18 @@ Clock.sectorClockImproved.paintArc = function (unitName,
   context.lineWidth = width;
   if (options.sweep) {
     context.beginPath();
-    context.strokeStyle = (options.second ? palette : palette.second).arc.done;
+    context.strokeStyle = palette.arc.done;
     var a = angle + fraction * 2 * Math.PI / hertz;
     context.arc(center.x, center.y, distance, a, a + 2 * Math.PI / hertz);
     context.stroke();
   } else {
     context.beginPath();
-    context.strokeStyle =
-        (options.second ? palette : palette.second).arc.remaining;
+    context.strokeStyle = palette.arc.remaining;
     context.arc(center.x, center.y, distance,
         angle, angle + 2 * Math.PI / hertz);
     context.stroke();
     context.beginPath();
-    context.strokeStyle = (options.second ? palette : palette.second).arc.done;
+    context.strokeStyle = palette.arc.done;
     context.arc(center.x, center.y, distance,
         angle, angle + fraction * 2 * Math.PI / hertz);
     context.stroke();
